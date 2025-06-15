bd0647586d9a3d8b5d08d1b1a862529c3f724e8b
Sudipofficial4 <officialsudip44@gmail.com>
2025-06-15 22:18:08 +0545

    app completed

--- lib/Mainscreen.dart
+++ lib/Mainscreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todoapp/widgets/Todolist.dart';
+import 'New.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,38 +13,129 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  void OnClick() {
+    showModalBottomSheet(
+      //backgroundColor: CupertinoColors.secondaryLabel,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: Container(
+            height: 200,
+            child: New(addtodo: addtodo),
+            padding: EdgeInsets.all(4),
+          ),
+        );
+      },
+    );
+  }
+
+  List<String> todo = [];
+
+  void addtodo({required String Todotext}) {
+    if (todo.contains(Todotext)) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Already Exists'),
+            content: Text("The text already exists"),
+            actions: [
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+
+                child: Text("close"),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    setState(() {
+      todo.insert(0, Todotext);
+    });
+    updatelocaldata();
+    Navigator.pop(context);
+  }
+
+  void updatelocaldata() async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('todo', todo);
+  }
+
+  void loaddata() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('todo', todo);
+    todo = (prefs.getStringList('todo') ?? []).toList();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    loaddata();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(child: Text('MENU')),
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Todo App'), 
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              splashColor: Colors.blueGrey,
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+
+        child: Icon(Icons.add),
+
+        onPressed: OnClick,
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blueGrey[900],
+              height: 200,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  "Todo App",
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ),
+            ),
+            ListTile(
               onTap: () {
-                showModalBottomSheet(
-                  backgroundColor: CupertinoColors.secondaryLabel,
-                  context: context,
-                  builder: (context) {
-                    return Container();
-                  },
+                launchUrl(Uri.parse("https://www.sudipkoirala44.com.np/"));
+              },
+              leading: Icon(Icons.person_2),
+              title: Text(
+                "Get In Touch",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                launchUrl(
+                  Uri.parse("https://www.facebook.com/sudip.koirala.415846"),
                 );
               },
-              child: Icon(Icons.add, color: Colors.cyan, size: 45),
+              leading: Icon(Icons.facebook),
+              title: Text(
+                "About me",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      body: InkWell(
-        onTap: () {
-          print('clicked');
-        },
-        child: Text('Some Text'),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Todo App'),
+        actions: [Padding(padding: const EdgeInsets.all(8.0))],
       ),
+      body: Todolist(todolist: todo, updatelocaldata: updatelocaldata),
     );
   }
 }
--- lib/New.dart
+++ lib/New.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
 class New extends StatefulWidget {
-  const New({super.key});
+  void Function({required String Todotext}) addtodo;
+
+  New({super.key, required this.addtodo});
 
   @override
   State<New> createState() => _NewState();
 }
 
 class _NewState extends State<New> {
+  TextEditingController todotext = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const Text("ADD TODO"),
+        TextField(
+          autofocus: true,
+          controller: todotext,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(4),
+            hintText: 'Write Your TODO Here',
+            //labelText: 'Username',
+            //icon: Icon(Icons.swipe_up_sharp),
+          ),
+          //style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (todotext.text.isNotEmpty) {
+              widget.addtodo(Todotext: todotext.text);
+            }
+
+            todotext.text = '';
+          },
+          child: Text("Add"),
+        ),
+      ],
+    );
   }
 }
